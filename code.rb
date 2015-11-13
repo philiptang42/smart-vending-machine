bcc68e5c0540dc864a70df22e1de699a5af09fda
philiptang42 <philiptang42@gmail.com>
2015-11-13 01:13:42 -0500

    step 2 solution

--- code.rb
+++ code.rb
@@ -1,8 +1,23 @@
-puts "What is the name of the item you're looking for?"
-item = gets.chomp
-puts "How many of that item would you like?"
-amount = gets.chomp.to_i
+require "pry"
 
-amount.times do
-  puts item
-end
+  puts "I'm a Vending Machine"
+  puts "What is the name of the item you're looking for?"
+  item = gets.chomp
+  puts "How many of that item would you like?"
+  amount = gets.chomp
+
+  if amount == "tons"
+    puts item
+    random_number = rand(14)
+    while random_number = rand(14) do
+      if random_number != 0
+        puts item
+      else
+        break
+      end
+    end
+  end
+
+  amount.to_i.times do
+    puts item
+  end
